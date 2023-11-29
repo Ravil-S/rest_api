e90582164d33cc5ee64a50235950763dab7b8c59
K1 <k1_email@email.email>
2023-11-29 20:48:15 +0400

    delete service method and tests

--- src/main/java/rest/service/CalculateService.java
+++ src/main/java/rest/service/CalculateService.java
@@ -12,40 +12,6 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 public class CalculateService {
 
-    public String getCharFreqJSON(String text) {
-
-        if (text == null) return null;
-        if (text.equals("")) return null;
-        if (text.length()>100) return null;
-
-            char[] chars = text.toCharArray();
-
-            HashMap<Character, Long> hashMap = new LinkedHashMap<>();
-
-            for (char ch : chars) {
-                if (!hashMap.isEmpty()) {
-                    if (hashMap.containsKey(ch)) {
-                        hashMap.replace(ch, hashMap.get(ch) + 1);
-                        continue;
-                    }
-                }
-                hashMap.put(ch, 1L);
-            }
-
-            Map<Character, Long> sortedMap = hashMap.entrySet().stream()
-                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            String jacksonData = null;
-            try {
-                jacksonData = objectMapper.writeValueAsString(sortedMap);
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-            }
-            return jacksonData;
-    }
-
     public Map<String, Long> getCharFreqMap(String text) {
 
         if (text == null) return null;
--- src/test/java/service/CalculateServiceTest.java
+++ src/test/java/service/CalculateServiceTest.java
@@ -13,46 +13,6 @@ public class CalculateServiceTest {
     private CalculateService calculateService = new CalculateService();
 
     @Test
-    void getCharFreqJSON(){
-        String input = "aaabbcddddfff###111%%%   66";
-        String output = "{\"d\":4,\"a\":3,\"f\":3,\"#\":3,\"1\":" +
-                "3,\"%\":3,\" \":3,\"b\":2,\"6\":2,\"c\":1}";
-
-        String json = calculateService.getCharFreqJSON(input);
-        Assertions.assertEquals(output, json);
-    }
-
-    @Test
-    void inputNull(){
-        String input = null;
-        String output = null;
-
-        String json = calculateService.getCharFreqJSON(input);
-        Assertions.assertEquals(output, json);
-    }
-
-    @Test
-    void inputVoid(){
-        String input = "";
-        String output = null;
-
-        String json = calculateService.getCharFreqJSON(input);
-        Assertions.assertEquals(output, json);
-    }
-
-    @Test
-    void lenghtOver100(){
-
-        String input = "a".repeat(101);
-        String output = null;
-
-        String json = calculateService.getCharFreqJSON(input);
-        Assertions.assertEquals(output, json);
-    }
-
-    //////// map ///////
-
-    @Test
     void getCharFreqMap(){
         String input = "fffffzzz";
         Map<String, Long> output = new HashMap<>();
